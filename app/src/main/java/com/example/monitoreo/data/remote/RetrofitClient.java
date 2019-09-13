33a9f88cb8869843b1a278d29d190b7505d3dd27
jemmaengz <juanemmanuel1202@gmail.com>
2019-09-13 12:48:15 -0500

    Add token condition on RetrofitClient Interceptor
    Add a condition to only add Authorization header when the token is set.
    
     En la rama development
     Cambios para hacer commit:
    	modificado: app/src/main/java/com/example/monitoreo/data/remote/RetrofitClient.java

--- app/src/main/java/com/example/monitoreo/data/remote/RetrofitClient.java
+++ app/src/main/java/com/example/monitoreo/data/remote/RetrofitClient.java
@@ -9,7 +9,7 @@ import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-class RetrofitClient {
+public class RetrofitClient {
 
     public static String token = "";
 
@@ -30,11 +30,13 @@ class RetrofitClient {
         return new OkHttpClient.Builder().addInterceptor(new Interceptor() {
             @Override
             public Response intercept(Chain chain) throws IOException {
-                Request originalRequest = chain.request();
-                Request authorizedRequest = originalRequest.newBuilder()
-                        .header("Authorization", token)
-                        .build();
-                return chain.proceed(authorizedRequest);
+                Request request = chain.request();
+                if (!token.isEmpty()) {
+                    request = request.newBuilder()
+                            .header("Authorization", token)
+                            .build();
+                }
+                return chain.proceed(request);
             }
         }).build();
     }
